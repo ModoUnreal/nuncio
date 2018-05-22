99521ad1425330f57dffbaa952d8b69db084a324
BadassBanana <alejandrohurtado888@gmail.com>
2018-05-22 23:20:50 +0100

    Fixed bugs in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from logging.handlers import RotatingFileHandler
 
 import logging
+import os
 
 
 db = SQLAlchemy()
