fa29c720b38890d6467dee6b90c87b42cb6405e8
BadassBanana <alejandrohurtado888@gmail.com>
2018-05-23 21:03:56 +0100

    Got bored, made bad joke

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-number-9-large'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'skrillexisdabestgeetaristeva' # Oh you wish this was the secret key.
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(base_dir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
