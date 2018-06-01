7cfbc73238f0aa77ea18a4c2023b06b0e26c19f4
BadassBanana <alejandrohurtado888@gmail.com>
2018-06-01 08:58:45 +0100

    Fixed some simple pagination and styling bugs

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class Config(object):
             'sqlite:///' + os.path.join(base_dir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 30
